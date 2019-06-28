9f6511721840d7f83dc33e8d7d40521a57752989
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-06-28 00:42:23 -0500

    added signup model and signup post to api

--- Gnomi.Web/ClientApp/src/app/login-signup/signup.component.ts
+++ Gnomi.Web/ClientApp/src/app/login-signup/signup.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { SignupFormModel } from '../models/signup_form.model';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  })
+};
 
 @Component({
   selector: 'signup',
   templateUrl: './signup.component.html',
 })
 export class SignupComponent {
+
+  constructor(private http: HttpClient) { }
+
   public signupEmail = '';
   public signupPassword = '';
 
@@ -12,5 +26,13 @@ export class SignupComponent {
   public initialSignup(email: string, password: string) {
     this.signupEmail = email;
     this.signupPassword = password;
+    this.postSignup(email, password);
   }
+
+  public postSignup(email: string, password: string) {
+    let payload = new SignupFormModel(email, password);
+    let body = JSON.stringify(payload);
+    let ob = this.http.post("https://localhost:44393/api/signup", payload, httpOptions).subscribe((data: any[]) => console.log(data));
+  }
+
 }
---
+++ Gnomi.Web/ClientApp/src/app/models/signup_form.model.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SignupFormModel = /** @class */ (function () {
+    function SignupFormModel(email, password) {
+        this.email = email;
+        this.password = password;
+    }
+    return SignupFormModel;
+}());
+exports.SignupFormModel = SignupFormModel;
+//# sourceMappingURL=signup_form.model.js.map
